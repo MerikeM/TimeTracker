04bbdac33e34168d13d4dc4adac24f3ea157aa78
Merike Meizner <merike.meizner@gmail.com>
2016-10-15 13:50:44 +0300

    taimeri töö nähtav, projektide nimed ei saa korduda

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,34 +2,39 @@ package Main; /**
  * Created by Merike on 03-Oct-16.
  */
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.*;
 import javafx.geometry.Insets;
 import javafx.stage.*;
 import javafx.scene.*;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
+import javafx.util.Duration;
+
 
 import java.util.ArrayList;
 
+
+
 public class Main extends Application{
 
-    ArrayList<Project> allProjects = new ArrayList<Project>();
+    ArrayList<Project> allProjects = new ArrayList<Project>(); //sisaldab kõiki loodud projekte
+    Timeline timeline;
 
-    Project projekt = new Project("projekt");
-
-
-    //ülemine menüü
+    //ülemise menüü elemendid
     Button timerButton = new Button("Taimer");
     Button entryButton = new Button("Lisa/muuda sissekandeid");
     Button statistikaButton = new Button("Statistika");
 
-    //stopperi osa
+    //stopperi osa elemendid
     Button startEndButton;
     Stopwatch stopwatch = new Stopwatch();
     Label timeLabel;
     ComboBox<String> projectDropDown = new ComboBox<>();
 
-    //projektide osa
+    //projektide osa elemendid
     Label totalTimeLabel = new Label(getTotalTimes(allProjects));
     TextField newProjectTextField = new TextField();
     Button addProjectButton = new Button("Lisa uus projekt");
@@ -41,16 +46,15 @@ public class Main extends Application{
         launch(args);
     }
 
+
+
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("lihtne taimer");
-        //ülemine menüü
-
 
         //stopperi osa
-
         projectDropDown.setMinWidth(120);
-
 
         timeLabel = new Label("Aeg: " + stopwatch.getLength());
 
@@ -64,10 +68,11 @@ public class Main extends Application{
 
         });
 
-        //projekti osa
-
+        //projektide osa
         addProjectButton.setOnAction(e -> addNewProject());
 
+
+        //LAYOUT
 
         //ülemine menüü
         HBox upperLayout = new HBox(20);
@@ -98,6 +103,7 @@ public class Main extends Application{
 
     }
 
+    // koostab sõne, mis sisaldab kõiki projekte ja nende koguaegu
     public String getTotalTimes(ArrayList<Project> list){
         String result = "";
         for (int i = 0; i < list.size(); i++) {
@@ -107,6 +113,9 @@ public class Main extends Application{
         }
         return result;
     }
+
+    //Sisendiks projekte sisaldav ArrayList ja otsitava projekti nimi.
+    //Väljastab otsitava nimega projekti.
 
     public Project findProjectByName(ArrayList<Project> list, String name){
         for (int i = 0; i < list.size(); i++) {
@@ -119,35 +128,60 @@ public class Main extends Application{
         return new Project("nimetu");
     }
 
+    //Lisab uue projekti. Projekti nimi pärineb vastavast TextField-ist
     public void addNewProject(){
         String projectName=newProjectTextField.getText();
         newProjectTextField.clear();
+
+        for (int i = 0; i < allProjects.size(); i++) {
+            Project p = allProjects.get(i);
+            if (p.getName().equals(projectName)){
+                AlertBox.display("Viga", "Sellise nimega projekt on olemas");
+                return;
+            }
+        }
+
         Project newProject = new Project(projectName);
         projectDropDown.getItems().add(newProject.toString());
         allProjects.add(newProject);
         totalTimeLabel.setText(getTotalTimes(allProjects));
     }
 
+    //Käivitab stopperi
     public void startStopwatch(){
         stopwatch.start();
         startEndButton.setText("Stopp");
+        projectDropDown.setDisable(true);
+        timeline = new Timeline(new KeyFrame(
+                Duration.millis(1000),
+                ae -> updateTimeLabel()));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
     }
 
+    //Lõpetab stopperi töö ja lisab selle aja valitud projektile
     public void stopStopwatch(){
         stopwatch.end();
         stopwatch.calcLength();
         Time entryTime = stopwatch.getLength();
-        timeLabel.setText("Aeg: " + entryTime);
         startEndButton.setText("Start");
+        projectDropDown.setDisable(false);
+        timeline.stop();
 
         String currentProjectName = projectDropDown.getValue();
         Project currentProject = findProjectByName(allProjects, currentProjectName);
         Entry currentEntry = new Entry(entryTime, new Date(13,10,2016));
         currentProject.newEntry(currentEntry);
-        
 
         totalTimeLabel.setText(getTotalTimes(allProjects));
 
+    }
+
+    //uuendab stopperi näitu
+    public void updateTimeLabel(){
+        Time currentLength = stopwatch.getCurrentLength();
+        String currentLengthString = currentLength.toString();
+        timeLabel.setText(currentLengthString);
     }
 
 
